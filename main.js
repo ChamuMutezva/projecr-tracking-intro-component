ae8ff9760e317bf5b6813d237c76e89af0e2345c
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-09-28 07:13:32 +0200

    background image layout/ text-transform to uppercase

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const modal = document.querySelector(".modal");
 console.log(openModal);
 
 openModal.addEventListener("click", function () {
+    this.src = "images/times-circle-regular.svg";
     modal.style.visibility = "visible";
 })
