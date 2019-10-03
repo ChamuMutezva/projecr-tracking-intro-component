00c0c71caf43f7a4c50b24d34c1a51fe44939782
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-10-03 23:07:36 +0200

    desktop final polish

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const openModal = document.querySelector(".toggleMenu");
-
+let isOpen = false;
 const modal = document.querySelector(".modal");
 console.log(openModal);
 
 openModal.addEventListener("click", function () {
+    if(isOpen){
+        this.src = "images/bars-solid.svg";
+        modal.style.visibility = "hidden";
+        isOpen = false;
+    } else {
     this.src = "images/times-circle-regular.svg";
     modal.style.visibility = "visible";
+    isOpen = true;
+    }
 })
 
-/*openModal.addEventListener("click", fav )
-
-function fav(e){
-    const target = e.target;
-    const closeIcon = document.querySelector(".fa-window-close");
-    console.log(closeIcon);    
-    target.classList.toggle(".fa-window-close");
-    target.classList.remove(".fa-bars");
-    modal.style.visibility = "visible";
-}*/
